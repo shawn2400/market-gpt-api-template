4bebacb1d8384a8e399a5118676cc028b7052386
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-24 19:35:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,9 @@ def home():
 
 if __name__ == "__main__":
     import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+
 
 
 
