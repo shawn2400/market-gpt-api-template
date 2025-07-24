ffac16effdb24a7b7384b0540e20bff07d7c0a53
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-24 20:07:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,8 @@ def home():
 
 # הפעלת השרת
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
 
 
 
