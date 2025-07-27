05b438f04fe568f3e45dca2adcd6743a946d7241
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-28 00:50:06 +0300

    Create binance_client.py

---
+++ utils/binance_client.py
@@ -0,0 +1,10 @@
+from binance.client import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_API_SECRET")
+
+client = Client(api_key, api_secret)
