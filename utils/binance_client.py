551a7037b1774e84095230c85f9b59a9cd90632b
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-28 02:00:22 +0300

    Update binance_client.py

--- utils/binance_client.py
+++ utils/binance_client.py
@@ -1,11 +1,10 @@
+# utils/binance_client.py
 import os
 from binance.client import Client
 from dotenv import load_dotenv
 
-# טוען משתני סביבה מקובץ .env
 load_dotenv()
 
-# יצירת לקוח Binance על בסיס מפתחות סודיים מה־env
 client = Client(
     api_key=os.getenv("BINANCE_API_KEY"),
     api_secret=os.getenv("BINANCE_API_SECRET")
