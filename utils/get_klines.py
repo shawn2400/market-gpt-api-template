7575add8f23e5841ec2a56ee3f6354c38d1954e8
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-28 01:03:59 +0300

    Update get_klines.py

--- utils/get_klines.py
+++ utils/get_klines.py
@@ -3,5 +3,7 @@ from utils.binance_client import client
 def get_klines(symbol, interval='15m', limit=100):
     try:
         return client.futures_klines(symbol=symbol, interval=interval, limit=limit)
-    except:
+    except Exception as e:
+        print(f"שגיאה בשליפת Klines עבור {symbol}: {e}")
         return []
+
