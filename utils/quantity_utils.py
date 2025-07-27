f8b9c073b8f9867bb057eb754a2fd2e0f2700112
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-27 20:14:47 +0300

    Update quantity_utils.py

--- utils/quantity_utils.py
+++ utils/quantity_utils.py
@@ -1,1 +1,10 @@
+# utils/quantity_utils.py
+def calculate_quantity(budget_usdt, entry_price, leverage, precision=3):
+    """
+    חישוב כמות לפי תקציב ב-USDT, מחיר כניסה ומינוף
+    """
+    if entry_price <= 0 or leverage <= 0:
+        return 0
+    raw_qty = (budget_usdt * leverage) / entry_price
+    return round(raw_qty, precision)
 
