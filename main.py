52e3a57340b7a0d987eb5eb84aa4625c159f44f3
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-28 03:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-
 import os
 import time
 import logging
@@ -144,6 +142,11 @@ def crypto_news():
     except Exception as e:
         logging.error(f"❌ שגיאה בשליפת חדשות: {e}")
         return jsonify({"status": "error", "message": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
