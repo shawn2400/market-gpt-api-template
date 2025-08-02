77f9dd68f1b39376f85fde3594bd1d9e06690514
shalom abecassis <shaharabecassis8@gmail.com>
2025-08-02 23:53:04 +0300

    🚀 החלפה סופית ל־ws_fallback והסרת get_price/get_live_price

--- utils/get_live_price.py
+++
@@ -1,9 +0,0 @@
-# utils/get_live_price.py
-from utils.binance_client import client
-
-def get_price(symbol: str) -> float:
-    """
-    Fetch the current live price for the given symbol using the Binance API client.
-    """
-    ticker = client.get_symbol_ticker(symbol=symbol)
-    return float(ticker['price'])
