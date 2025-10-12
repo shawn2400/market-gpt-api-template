8e417d9f30b5ee968ee1af102482801b093439a3
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-10-13 02:45:47 +0300

    Create sm.sh

---
+++ sm.sh
@@ -0,0 +1,15 @@
+cat > /app/sm.sh <<'SH'
+sm() {
+  BODY="$1"
+  TS="$(date +%s)"
+  NONCE="$(openssl rand -hex 8)"
+  SIG="$(printf "%s.%s.%s" "$TS" "$NONCE" "$BODY" | \
+        openssl dgst -sha256 -mac HMAC -macopt hexkey:$SECRET_HEX | awk '{print $2}')"
+  curl -sS -X POST "$HOST/manage-once" \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "Content-Type: application/json" \
+    -H "X-Timestamp: $TS" -H "X-Nonce: $NONCE" -H "X-Signature: $SIG" \
+    --data "$BODY"
+}
+SH
+. /app/sm.sh
