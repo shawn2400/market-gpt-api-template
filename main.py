412553e29387fda7a12ee573592177dc429c6233
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-10-17 02:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # main.py
 # -*- coding: utf-8 -*-
 from __future__ import annotations
@@ -2418,8 +2419,6 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", "10000"))
     reload_ = os.getenv("UVICORN_RELOAD", "0").lower() in ("1", "true", "yes", "on")
     uvicorn.run("main:app", host=host, port=port, reload=reload_, log_level=LOG_LEVEL.lower())
-
-
 
 
 
